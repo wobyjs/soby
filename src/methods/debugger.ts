e7759646e908efc6394ee3631981c25d66e4eb82
wongchichong <wongchichong@gmail.com>
2025-09-12 16:59:33 +0800

    Bump version

--- src/methods/debugger.ts
+++ src/methods/debugger.ts
@@ -1,10 +1,17 @@
 export const DEBUGGER = { debug: false, test: false }
 
 export class Stack extends Error {
-    constructor(message: string) {
+    constructor(message = '', startIndex = 0) {
         super(message)
         this.name = "Stack" // Change the error class name
         Object.setPrototypeOf(this, Stack.prototype) // Ensure correct prototype chain
+
+        if (this.stack) {
+            const stackLines = this.stack.split('\n')
+            const header = stackLines[0] // usually "Stack: message"
+            const body = stackLines.slice(1 + startIndex) // drop frames before `startIndex`
+            this.stack = [header, ...body].join('\n')
+        }
     }
 }
 
